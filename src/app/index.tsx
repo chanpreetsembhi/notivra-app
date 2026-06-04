48f1a9cf384f005aa6fcba422c09c70d688ab42e
chanpreet Singh <chanpreetsingh73553@gmail.com>
2026-06-04 17:05:41 +0530

    project created

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl font-bold">Notivra App</Text>
+    </View>
+  );
+}
